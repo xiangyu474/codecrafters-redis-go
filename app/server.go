1a5f2b967ec07b224775584407b75538d19c5317
Xiang Yu Zhang <xiangyu474@gmail.com>
2025-02-13 16:55:14 +0800

    respond to multiple PINGs

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
@@ -14,20 +15,44 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	fmt.Print("Listening on")
-	// conn, err = l.Accept()
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	fmt.Println("Listening on 6379")
+
+	// conn, err := l.Accept()
+	for {
+		// ⚠️ 每次循环都 `Accept()` 一个新的连接，避免死循环
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Accepted a connection!")
+		handleConnection(conn)
 	}
-	_, err = conn.Write([]byte("+PONG\r\n"))
 
 }
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			return
+		}
+
+		fmt.Println("Received message: ", msg)
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			return
+		}
+	}
+}
+
+// Test: ncat 127.0.0.1 6379
